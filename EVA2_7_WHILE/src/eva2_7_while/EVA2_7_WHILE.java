614a977c8bc09cdcc145303b203444e9b14d5b8a
FPCucoPuffs <L24550350@chihuahua2.tecnm.mx>
2024-10-15 17:51:35 -0600

    Add files via upload

---
+++ EVA2_7_WHILE/src/eva2_7_while/EVA2_7_WHILE.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva2_7_while;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA2_7_WHILE {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int val = 1;
+        while(val <= 10){
+            System.out.print(val + " - ");
+            val++;
+        }
+        val = 1;
+        do{
+            System.out.print(val + " - ");
+            val++;
+        }while(val <= 10);
+    }
+                    
+        }
+    
+    
+
