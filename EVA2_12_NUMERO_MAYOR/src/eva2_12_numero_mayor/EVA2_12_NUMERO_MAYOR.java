2492a4b7c979303cee2cd3d476862135398d3848
FPCucoPuffs <L24550350@chihuahua2.tecnm.mx>
2024-10-16 17:51:12 -0600

    Add files via upload

---
+++ EVA2_12_NUMERO_MAYOR/src/eva2_12_numero_mayor/EVA2_12_NUMERO_MAYOR.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva2_12_numero_mayor;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA2_12_NUMERO_MAYOR {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int num, num2;
+        Scanner captu = new Scanner(System.in);
+        do{
+       
+        System.out.println("ESCRIBE UN NUMERO:");
+        num = captu.nextInt();
+        captu.nextLine();
+        }while(!(num == 5));
+        
+        
+        
+        
+        
+        }
+}
+
